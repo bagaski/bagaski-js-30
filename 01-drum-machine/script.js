f705b5f4025c960ee99d0fcf067a28112d162fc4
Yiorgos Bagakis <ybagakis@gmail.com>
2020-12-04 09:21:16 +0100

    all bugs fix

--- 01-drum-machine/script.js
+++ 01-drum-machine/script.js
@@ -1,24 +1,41 @@
- function playsound(e) {
+
+ window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; //if you hit a key without audio
   audio.currentTime = 0; //rewind to the start
   audio.play();
   key.classList.add('playing');
-}
+});
 
 function removeTransition(e) {
-<<<<<<< HEAD
-  /*if (e.propertyName !== 'transform') return;
 
-*/key.classList.remove('playing');
-console.log('Transition ended');
-=======
-  if (e.propertyName !== 'transform') return;
-  this.classList.remove('playing');
->>>>>>> 2bea8999941a3b4a49b762dfbc46bf0a6e63553a
+//if (e.propertyName !== 'transform') return;
+
+this.classList.remove('playing');
+
+
 }
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playsound);
+
+ /*
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);*/
