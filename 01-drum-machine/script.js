7fd007e5afaafad8b9fb22b9e1233e2d1d3cd1ef
Yiorgos Bagakis <ybagakis@gmail.com>
2020-12-06 19:29:15 +0100

    add click event & better css

--- 01-drum-machine/script.js
+++ 01-drum-machine/script.js
@@ -1,41 +1,33 @@
 
- window.addEventListener('keydown', function(e) {
+const keys = document.querySelectorAll('.key');
+//const.audios = document.querySelectorAll('audio');
+
+function playDrum(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; //if you hit a key without audio
+  key.classList.add('playing');
   audio.currentTime = 0; //rewind to the start
   audio.play();
-  key.classList.add('playing');
-});
+};
 
+//function for key set to its initial state
 function removeTransition(e) {
-
-//if (e.propertyName !== 'transform') return;
-
-this.classList.remove('playing');
-
-
+  this.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll('.key');
+//function for clicking on the drum
+function clickDrum(e) {
+  const keyClicked = e.target.dataset.key //the key number i clicked on
+  const audiop = document.querySelector(`audio[data-key="${keyClicked}"]`);
+  this.classList.add('playing');
+  if (!audiop) return; //if you hit a key without audio
+  audiop.play();
+  audiop.currentTime = 0;
+
+  console.log(keyClicked);
+}
+
+window.addEventListener('keydown', playDrum);
+keys.forEach(key => key.addEventListener('click', clickDrum));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
- /*
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
-
-  function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-  }
-
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);*/
