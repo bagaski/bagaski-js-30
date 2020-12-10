fd672f419f445cb3be56a9427d0ea3020221661e
Yiorgos Bagakis <ybagakis@gmail.com>
2020-12-10 22:57:05 +0100

    tiny css + js updates 2

--- 02-clock/script.js
+++ 02-clock/script.js
@@ -1,16 +1,18 @@
 function test() {
   const sec = document.querySelector('.hand-sec');
   sec.style.transform = 'rotate(45deg)';
-  return sec.style.transform;
 }
 
-setIntweval(test(),1000)
+setInterval(test,1000)
 
 
-var myVar = setInterval(myTimer, 1000);
 
+/*
 function myTimer() {
   var d = new Date();
   var t = d.toLocaleTimeString();
   document.getElementById("demo").innerHTML = t;
 }
+
+var myVar = setInterval(myTimer, 1000);
+*/
