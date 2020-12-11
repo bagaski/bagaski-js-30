5cef15a42897141c5ff38521782c7388fe3c674d
Yiorgos Bagakis <ybagakis@gmail.com>
2020-12-11 23:07:56 +0100

    clock & digital ready

--- 02-clock/script.js
+++ 02-clock/script.js
@@ -1,18 +1,17 @@
+const handSec = document.querySelector('.hand-sec');
+const handMin = document.querySelector('.hand-min');
+const handHr = document.querySelector('.hand-hr');
+const digitClock = document.querySelector('.digit-clock');
+
 function test() {
-  const sec = document.querySelector('.hand-sec');
-  sec.style.transform = 'rotate(45deg)';
+  const time = new Date();
+  const seconds = time.getSeconds();
+  const minutes = time.getMinutes();
+  const hours = time.getHours();
+  handSec.style.transform = `rotate(${((seconds+15)/60)*360}deg)`;
+  handMin.style.transform = `rotate(${((minutes+15)/60)*360}deg)`;
+  handHr.style.transform = `rotate(${((hours+15)/12)*360}deg)`;
+  digitClock.textContent = ((hours > 10) ? hours : '0' + hours) + " : " + ((minutes > 10) ? minutes : '0' + minutes) + " : " + ((seconds > 10) ? seconds : '0' + seconds)
 }
 
-setInterval(test,1000)
-
-
-
-/*
-function myTimer() {
-  var d = new Date();
-  var t = d.toLocaleTimeString();
-  document.getElementById("demo").innerHTML = t;
-}
-
-var myVar = setInterval(myTimer, 1000);
-*/
+setInterval(test, 1000);
