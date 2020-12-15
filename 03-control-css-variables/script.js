ee06e50de565df04bc2b0d1a629e277955da1f44
Yiorgos Bagakis <ybagakis@gmail.com>
2020-12-15 22:53:33 +0100

    js h1 color control

--- 03-control-css-variables/script.js
+++ 03-control-css-variables/script.js
@@ -1,1 +1,37 @@
-console.log('hello world')
+
+
+const selectColor = document.querySelectorAll('input')[1]
+const h1 = document.querySelector('h1');
+
+
+function updateColor(e) {
+  h1.style.color = e.target.value;
+  console.log(e.target.value)
+}
+
+selectColor.addEventListener('change', updateColor);
+
+
+
+/*
+const controls = document.querySelectorAll('input');
+
+
+
+function handler(event) {
+  const colour = element.style.setProperty("--my-variable", ${event.target});
+}
+
+controls.forEach((control) => { control.addEventListener('change', handler(event))
+
+});
+
+
+const selectElement = document.querySelector('.ice-cream');
+
+selectElement.addEventListener('change', (event) => {
+  const result = document.querySelector('.result');
+  result.textContent = `You like ${event.target.value}`;
+});
+
+*/
