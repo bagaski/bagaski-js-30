d90c0cd03b3a58e223a774bdad269331960bac1d
Yiorgos Bagakis <ybagakis@gmail.com>
2020-12-16 22:12:21 +0100

    js space & blur controls

--- 03-control-css-variables/script.js
+++ 03-control-css-variables/script.js
@@ -1,6 +1,9 @@
 
 
-const selectColor = document.querySelectorAll('input')[1]
+const selectColor = document.querySelectorAll('input')[1];
+const selectBlur = document.querySelectorAll('input')[0];
+const selectSpace = document.querySelectorAll('input')[2];
+const img = document.querySelector('img');
 const h1 = document.querySelector('h1');
 const display = document.querySelector('.display');
 
@@ -8,11 +11,27 @@ const display = document.querySelector('.display');
 function updateColor(e) {
   h1.style.color = e.target.value;
   display.style.borderColor = e.target.value;
-  display.style.backgroundColor = e.target.value
-  console.log(e.target.value)
+  display.style.backgroundColor = e.target.value;
+  console.log(e.target.value);
+}
+
+function updateBlur(e) {
+  const f = e.target.value
+  img.style.filter = `blur(${f + 'px'})`;
+
+}
+
+function updateSpace(e) {
+  display.style.borderWidth = e.target.value + 'px';
+
 }
 
 selectColor.addEventListener('change', updateColor);
+selectBlur.addEventListener('change', updateBlur);
+selectSpace.addEventListener('change', updateSpace);
+selectColor.addEventListener('mousemove', updateColor);
+selectBlur.addEventListener('mousemove', updateBlur);
+selectSpace.addEventListener('mousemove', updateSpace);
 
 
 
